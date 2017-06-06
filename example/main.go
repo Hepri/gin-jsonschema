1fad743c4d00b6147b3247cab5499d017bae2d28
Sergey Shcherbina <trytoheal@gmail.com>
2017-06-07 04:57:53 +0500

    added example

---
+++ example/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+
+	"github.com/Hepri/gin-jsonschema"
+)
+
+func handlerFunc(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+var testSchema string = `
+{
+    "title": "Test Schema",
+    "type": "object",
+    "properties": {
+        "value": {
+            "type": "integer"
+        }
+    },
+    "required": ["value"]
+}
+`
+
+func main() {
+	r := gin.Default()
+	// wrap handler, all invalid json schema requests will produce Bad Request
+	r.POST("/", schema.ValidateUsingSchemaString(handlerFunc, testSchema))
+	r.Run(":8080")
+}
